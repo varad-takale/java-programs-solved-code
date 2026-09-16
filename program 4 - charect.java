68661ca17bee04e95b64d988b1dd328254763e6d
varad raju takale <varadtakale1706@gmail.com>
2026-09-16 10:49:55 +0530

    Add files via upload

---
+++ program 4 - charect.java
@@ -0,0 +1,20 @@
+public class charect {
+
+    public static void main(String[] args) {
+        char ch = 'v';
+        int number = ch;
+        char ch2 = 'a';
+        int number2 = ch2;
+        char ch3 = 'r';
+        int number3 = ch3;
+        char ch4 = 'a';
+        int number4 = ch4;
+        char ch5 = 'd';
+        int number5 = ch5;
+        System.out.println(number);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+    }
+}
