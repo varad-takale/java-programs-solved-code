8c106f0b94955b487920eed2d058a0c1ac97633d
varad raju takale <varadtakale1706@gmail.com>
2026-09-17 20:08:37 +0530

    Add files via upload

---
+++ program 5 - adult.java
@@ -0,0 +1,11 @@
+public class adult{
+    public static void main (String args[]){
+        int age= 23;
+        if (age>=18) 
+            {System.out.println("adult : vote, drive ");
+            
+        }else{
+            System.out.println("not adult");
+        }
+    }
+}
