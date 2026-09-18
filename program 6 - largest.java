224b55123bd654ec7b6a7df03d741eb6badd2fa7
varad raju takale <varadtakale1706@gmail.com>
2026-09-18 14:06:12 +0530

    Add files via upload

---
+++ program 6 - largest.java
@@ -0,0 +1,13 @@
+public class largest {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        if (a>=b) {
+            System.out.println("a is largest of 2");
+            
+        }else{
+            System.out.println("b is largest of 2");
+        }
+    }
+}
