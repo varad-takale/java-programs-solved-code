e0f8a073aa5a07972e46ae10db6c1c965b5a3f51
varad raju takale <varadtakale1706@gmail.com>
2026-09-19 10:47:59 +0530

    Add files via upload

---
+++ program 7 - evenodd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class evenodd {
+
+    public static void main(String[] args) {
+       // Scanner sc = new Scanner(System.in);
+      //  int number = sc.nextInt();
+         int number = 14;
+        if (number % 2 == 0) {
+            System.out.println(number +" : number is even");
+
+            
+        }else {System.out.println(number +" : number is odd");}
+    }
+}
