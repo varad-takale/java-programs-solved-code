75c5860db767b9f312eeadc13bbcf2cbe87f2de1
varad raju takale <varadtakale1706@gmail.com>
2026-09-22 10:09:43 +0530

    Add files via upload

---
+++ program 9 - largest3.java
@@ -0,0 +1,27 @@
+public class largest3 {
+
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 29;
+        int c = 15;
+
+        if (a>b && a>c)
+             {
+            System.out.println("a is largest of 3");
+            
+        }
+        else if 
+            (b>c)
+            {
+               System.out.println("b is largest of 3");
+            }
+            else
+                {
+                System.out.println("c is largest of 3");
+            }
+        
+
+    
+            
+        }
+    }
