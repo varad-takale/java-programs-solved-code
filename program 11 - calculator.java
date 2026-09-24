b3f6b53b1040f13470581a7c22d2fb801cac8561
varad raju takale <varadtakale1706@gmail.com>
2026-09-24 11:04:48 +0530

    Add files via upload

---
+++ program 11 - calculator.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class calculator {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter a number :" );
+        int a = sc. nextInt();
+        System.out.println("enter b number : ");
+        int b = sc.nextInt();
+        System.out.println("enter a operator");          
+        char operator = sc.next().charAt(0);
+        
+        switch (operator) {
+            case '+':System.out.println(a+b);break;
+            case '-':System.out.println(a-b);break;
+            case '*':System.out.println(a*b);break;
+            case '/':System.out.println(a/b);break;
+            case '%':System.out.println(a%b);break;
+            default: System.out.println("not that advanced");
+                break;
+        }
+    }
+}
